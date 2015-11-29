f1d5a52da429f511948a385af089a937e2a16eb2
Kilbourne <luca.castellone@gmail.com>
2015-11-29 12:58:27 +0100

    [STree] Added stree 'melarido' in web/app/themes/melarido

---
+++ web/app/themes/melarido/404.php
@@ -0,0 +1,14 @@
+ <div class="page-wrapper wrapper wrapper--wide" role="document" style="text-align:center;">
+    <header class="banner striscia striscia1">
+  <div class="header-wrapper">
+    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><img src="<?php echo get_stylesheet_directory_uri().'/dist/images/logo.png'; ?>" alt=""></a>
+  </div>
+  </header>
+
+
+<div class="alert alert-warning" style="font-size: 2rem;
+margin-top: 1.5rem;">
+  La pagina che cerchi non esiste. Clicca sul logo per tornare al sito.
+</div>
+
+    </div><!-- /.wrap -->
