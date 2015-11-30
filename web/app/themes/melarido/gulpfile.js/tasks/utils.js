95a0f205e7736db4a16eb51a236f107da7b756ce
Kilbourne <luca.castellone@gmail.com>
2015-11-30 02:01:17 +0100

    hgf

--- web/app/themes/melarido/gulpfile.js/tasks/utils.js
+++
@@ -1,8 +0,0 @@
-var gulp        = require('gulp')
-  , del         = require('del')  
-  , config      = require('../gulpconfig').util
-  ;
-
-// ### Clean
-// `gulp clean` - Deletes the build folder entirely.
-gulp.task('clean', del.bind(null, config.clean));
